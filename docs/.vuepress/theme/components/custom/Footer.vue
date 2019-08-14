e1eec9d625fc4b61315946e3ae0cdb6aead5241a
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-08-13 23:56:53 -0400

    Built out the site footer and styles.

--- docs/.vuepress/theme/components/custom/Footer.vue
+++ docs/.vuepress/theme/components/custom/Footer.vue
@@ -1,6 +1,71 @@
 <template>
   <footer class="page-footer">
-    <p>This is the page footer.</p>
+    
+    <div class="page-footer__links">
+      <div class="inner flex flex-wrap -mx-2">
+        <div class="w-full sm:w-1/2 md:w-1/4 px-2">
+          <ul>
+            <li><h5>Products</h5></li>
+            <li><a href="#">Kong</a></li>
+            <li><a href="#">Kong Enterprise</a></li>
+            <li><a href="#">Kong Cloud</a></li>
+            <li><a href="#">Subscriptions</a></li>
+          </ul>
+        </div>
+        <div class="w-full sm:w-1/2 md:w-1/4 px-2">
+          <ul>
+            <li><h5>Resources</h5></li>
+            <li><a href="#">Enterprise Support</a></li>
+            <li><a href="#">Documentation</a></li>
+            <li><a href="#">Partners</a></li>
+            <li><a href="#">Webinars</a></li>
+            <li><a href="#">Ebooks</a></li>
+            <li><a href="#">Meetups</a></li>
+          </ul>
+        </div>
+        <div class="w-full sm:w-1/2 md:w-1/4 px-2">
+          <ul>
+            <li><h5>Company</h5></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Investors</a></li>
+            <li><a href="#">News</a></li>
+            <li>
+              <a href="#" class="badge-wrap">
+                Careers <span class="badge">Hiring!</span>
+              </a>
+            </li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div class="w-full sm:w-1/2 md:w-1/4 px-2">
+          <ul>
+            <li><h5>Open Source</h5></li>
+            <li><a href="#">Mockbin</a></li>
+            <li><a href="#">API Embed</a></li>
+            <li><a href="#">Unirest</a></li>
+            <li><a href="#">Guardian JS</a></li>
+            <li><a href="#">OAuth Bible</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="page-footer__sublinks">
+      <div class="inner flex flex-wrap -mx-2">
+        <div class="w-full md:w-1/2 px-2">
+          <ul>
+            <li class="copyright">&copy; Kong, Inc. {{new Date().getFullYear()}}</li>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Sitemap</a></li>
+          </ul>
+        </div>
+        <div class="w-full md:w-1/2 px-2">
+
+        </div>
+      </div>
+    </div>
+
   </footer>
 </template>
 
@@ -9,10 +74,3 @@ export default {
   
 }
 </script>
-
-<style lang="scss">
-// temporary
-.page-footer {
-  text-align: center;
-}
-</style>
