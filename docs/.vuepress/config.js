4077755dccad0172c513d77af5ab204e1682dfbd
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-08-22 11:03:15 -0400

    Attempting to implement polyfills for some IE11 bugs.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -171,26 +171,5 @@ module.exports = {
         grid: true
       })
     ]
-  },
-  chainWebpack: config => {
-    const svgRule = config.module.rule('svg');
-
-    svgRule.uses.clear();
-    svgRule
-      .oneOf('external')
-      .resourceQuery(/external/)
-      .use('url')
-      .loader('url-loader')
-      .options({
-        limit: 10000,
-        name: 'public/images/[name].[hash:7].[ext]'
-      })
-      .end()
-      .end()
-      .oneOf('normal')
-      .use('raw')
-      .loader('raw-loader')
-      .end()
-      .end();
   }
 };
