7b211ebeeea98449f40ec3e0af6d5643dc9ee69c
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-08-22 12:06:01 -0400

    Switched to some different polyfill methods and node modules to see if this addresses the IE polyfill issues.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -192,7 +192,7 @@ module.exports = {
             "@babel/preset-env",
             {
               useBuiltIns: "usage",
-              corejs: 2,
+              corejs: 3,
               targets: {
                 ie: 11
               }
