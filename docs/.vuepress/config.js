734b9b92e492992541682527cacefa394e761e2f
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-08-22 15:54:33 -0400

    Logo fix for IE11.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -202,10 +202,6 @@ module.exports = {
               }
             }
           ]
-        ],
-        plugins: [
-          "@babel/plugin-proposal-object-rest-spread",
-          "babel-plugin-transform-es2015-arrow-functions"
         ]
       })
   }
