4ba9e1c42e7199a02d4360233fbe68edc260ca59
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-08-24 17:52:09 -0400

    Added a simple Node script that automatically creates the Netlify redirects based on whatever the latest version of the product is.

---
+++ setup-redirects/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const latestSemver = require("latest-semver");
+const releases = "./docs/.vuepress/public/releases.json";
+const tomlFile = "./netlify.toml";
+
+// read the releases file so we can parse the data
+fs.readFile(releases, "utf8", (err, data) => {
+  if (err) throw err;
+
+  // find the latest version in the releases JSON file
+  const latest = latestSemver(JSON.parse(data));
+
+  // setup the content template
+  const template = `# Docs redirect
+[[redirects]]
+from = "/docs/"
+to = "/docs/${latest}/"
+status = 301
+force = false
+
+# Install redirect
+[[redirects]]
+from = "/install/"
+to = "/install/${latest}/"
+status = 301
+force = false`;
+
+  // write our redirects to the TOML file
+  fs.writeFile(tomlFile, template, err => {
+    if (err) throw err;
+  });
+});
