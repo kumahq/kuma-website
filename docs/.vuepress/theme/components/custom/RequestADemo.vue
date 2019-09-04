a2c164f9bd9a822e881baff907a949d55c466e68
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-09-04 18:14:32 -0400

    Took a much more rudimentary approach with the forms since they aren't using modern tech to start with. Did away with the axios approach altogether.

--- docs/.vuepress/theme/components/custom/RequestADemo.vue
+++ docs/.vuepress/theme/components/custom/RequestADemo.vue
@@ -76,38 +76,66 @@
       </div>
 
       <div class="demo-request-form w-full sm:w-1/2 px-4">
-        <form action="" class="sticky">
+        <!-- 
+          This form uses Web-To-Lead by Salesforce,
+          which is a bit dated.
+          
+          @todo Find a solution that allows us to use
+          modern form handlers and axios or fetch.
+          The endpoint is currently not setup for
+          this approach and will not pass data
+          properly.
+        -->
+        <form action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST" class="sticky">
+          <input type="hidden" name="oid" value="00D41000000WdaQ">
+          <input type="hidden" name="retURL" value="http://go.konghq.com/l/392112/2019-09-03/bjz6ys">
+          <input type="hidden" name="Lead_Source_Detail" id="Lead_Source_Detail__c" value="Kuma Request a Demo">
+          <input type="hidden" name="lead_source" id="lead_source" value="Web">
+          <input type="hidden" name="Lead_Record_Type" id="RecordType" value="0121K000001QQgX">
+          <input type="hidden" name="utm_source" id="utm_source__c" value="">
+          <input type="hidden" name="utm_ad_group" id="utm_ad_group__c" value="">
+          <input type="hidden" name="utm_campaign" id="utm_campaign__c" value="">
+          <input type="hidden" name="utm_content" id="utm_content__c" value="">
+          <input type="hidden" name="utm_medium" id="utm_medium__c" value="">
+          <input type="hidden" name="utm_term" id="utm_term__c" value="">
+
+          <!-- debugging only -->
+          <input type="hidden" name="debug" value="1">
+          <input type="hidden" name="debugEmail" value="maria@konghq.com">
+
           <div class="flex flex-wrap -mx-4">   
             <div class="w-full md:w-1/2 px-4">
-              <label for="input-firstname">First Name</label>
-              <input type="text" name="firstname" id="input-firstname">
+              <label for="first_name">First Name</label>
+              <input  id="first_name" maxlength="40" name="first_name" size="20" type="text" />
             </div>
             <div class="w-full md:w-1/2 px-4">
-              <label for="input-lastname">Last Name</label>
-              <input type="text" name="lastname" id="input-lastname">
+              <label for="last_name">Last Name</label>
+              <input  id="last_name" maxlength="80" name="last_name" size="20" type="text" />
             </div>
             <div class="w-full md:w-1/2 px-4">
-              <label for="input-workemail">Work Email</label>
-              <input type="text" name="workemail" id="input-workemail">
+              <label for="email">Email</label>
+              <input id="email" maxlength="80" name="email" size="20" type="email" />
             </div>
             <div class="w-full md:w-1/2 px-4">
-              <label for="input-phone">Phone</label>
-              <input type="text" name="phone" id="input-phone">
+              <label for="company">Company</label>
+              <input id="company" maxlength="40" name="company" size="20" type="text" />
             </div>
             <div class="w-full md:w-1/2 px-4">
-              <label for="input-jobtitle">Job Title</label>
-              <input type="text" name="jobtitle" id="input-jobtitle">
+              <label for="phone">Phone</label>
+              <input id="phone" maxlength="40" name="phone" size="20" type="text" />
             </div>
             <div class="w-full md:w-1/2 px-4">
-              <label for="input-companyname">Company Name</label>
-              <input type="text" name="companyname" id="input-companyname">
+              <label for="title">Title</label>
+              <input id="title" maxlength="40" name="title" size="20" type="text" />
             </div>
             <div class="w-full px-4">
-              <label for="input-comments">How can we help you?</label>
-              <textarea name="comments" id="input-comments"></textarea>
+              <label for="00N4100000PN0Nm">How can we help you?</label>
+              <textarea id="00N4100000PN0Nm" name="00N4100000PN0Nm" rows="3" type="text" wrap="soft"></textarea>
             </div>
             <div class="w-full px-4 mt-4">
-              <button class="btn btn--bright btn--spaced btn--block">Request Demo</button>
+              <button type="submit" name="submit" class="btn btn--bright btn--spaced btn--block">
+                Request Demo
+              </button>
             </div>
           </div>
         </form>
--- docs/.vuepress/theme/global-components/NewsletterForm.vue
+++ docs/.vuepress/theme/global-components/NewsletterForm.vue
@@ -1,82 +1,46 @@
 <template>
   <div class="form-wrapper">
-    <form class="form-horizontal" @submit.prevent="submitForm">
 
-      <!-- hidden fields -->
-      <input v-model="formData.oid" type="hidden" name="oid" value="00D41000000WdaQ">
-      <input v-model="formData.retURL" type="hidden" name="retURL" value="http://go.konghq.com/l/392112/2019-09-03/bjz6yv">
-      <input v-model="formData.Lead_Source_Detail" type="hidden" name="Lead_Source_Detail" id="Lead_Source_Detail__c" value="Kuma Email List Signup">
-      <input v-model="formData.lead_source" type="hidden" name="lead_source" id="lead_source" value="Web">
-      <input v-model="formData.Lead_Record_Type" type="hidden" name="Lead_Record_Type" id="RecordType" value="0121K000001QQgX">
-      <input v-model="formData.utm_source" type="hidden" name="utm_source" id="utm_source__c" value="">
-      <input v-model="formData.utm_ad_group" type="hidden" name="utm_ad_group" id="utm_ad_group__c" value="">
-      <input v-model="formData.utm_campaign" type="hidden" name="utm_campaign" id="utm_campaign__c" value="">
-      <input v-model="formData.utm_content" type="hidden" name="utm_content" id="utm_content__c" value="">
-      <input v-model="formData.utm_medium" type="hidden" name="utm_medium" id="utm_medium__c" value="">
-      <input v-model="formData.utm_term" type="hidden" name="utm_term" id="utm_term__c" value="">
+    <!-- 
+      This form uses Web-To-Lead by Salesforce,
+      which is a bit dated.
+      
+      @todo Find a solution that allows us to use
+      modern form handlers and axios or fetch.
+      The endpoint is currently not setup for
+      this approach and will not pass data
+      properly.
+     -->
 
-      <!-- debugging -->
-      <input v-model="formData.debug" type="hidden" name="debug" value="1">
-      <input v-model="formData.debugEmail" type="hidden" name="debugEmail" value="maria@konghq.com">
+    <form class="form-horizontal" action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST">
+      <input type="hidden" name="oid" value="00D41000000WdaQ">
+      <input type="hidden" name="retURL" value="http://go.konghq.com/l/392112/2019-09-03/bjz6yv">
+      <input type="hidden" name="Lead_Source_Detail" id="Lead_Source_Detail__c" value="Kuma Email List Signup">
+      <input type="hidden" name="lead_source" id="lead_source" value="Web">
+      <input type="hidden" name="Lead_Record_Type" id="RecordType" value="0121K000001QQgX">
+      <input type="hidden" name="utm_source" id="utm_source__c" value="">
+      <input type="hidden" name="utm_ad_group" id="utm_ad_group__c" value="">
+      <input type="hidden" name="utm_campaign" id="utm_campaign__c" value="">
+      <input type="hidden" name="utm_content" id="utm_content__c" value="">
+      <input type="hidden" name="utm_medium" id="utm_medium__c" value="">
+      <input type="hidden" name="utm_term" id="utm_term__c" value="">
+
+      <!-- debugging only - hide for production -->
+      <input type="hidden" name="debug" value="1">
+      <input type="hidden" name="debugEmail" value="maria@konghq.com">
 
       <!-- user fields -->
       <label for="email" class="sr-only">Email</label>
-      <input v-model="formData.email" id="email" maxlength="80" name="email" size="20" type="email" />
+      <input  id="email" maxlength="80" name="email" size="20" type="text" />
 
-      <button type="submit" class="btn btn--bright">
+      <button type="submit" name="submit" class="btn btn--bright">
         Join Newsletter
       </button>
     </form>
+
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-
-export default {
-  data() {
-    return {
-
-      formData: {
-        // hidden fields
-        oid: '00D41000000WdaQ',
-        retURL: 'http://go.konghq.com/l/392112/2019-09-03/bjz6yv',
-        Lead_Source_Detail: 'Kuma Email List Signup',
-        lead_source: 'Web',
-        Lead_Record_Type: '0121K000001QQgX',
-        utm_source: '',
-        utm_ad_group: '',
-        utm_campaign: '',
-        utm_content: '',
-        utm_medium: '',
-        utm_term: '',
-
-        // debug fields
-        debug: 1,
-        debugEmail: 'maria@konghq.com',
-
-        // user fields
-        email: ''
-      }
-    }
-  },
-  methods: {
-    submitForm(ev) {
-      // console.log(this.formData)
-      axios.post( 'https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8', {
-        crossDomain: true,
-        data: JSON.stringify(this.formData),
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      })
-      .then(res => {
-        console.log(res.status, res.data)
-      })
-      .catch(err => {
-        console.error(err)
-      })
-    }
-  }
-}
+export default {}
 </script>
