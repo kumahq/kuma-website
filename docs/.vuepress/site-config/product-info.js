0ead9ba0e0f06453cc0d1d8171e7b061a3157ab2
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-09-04 22:00:10 -0400

    Updated the meta description and the Twitter handle.

--- docs/.vuepress/site-config/product-info.js
+++ docs/.vuepress/site-config/product-info.js
@@ -7,8 +7,8 @@
 
 module.exports = {
   title: "Kuma",
-  description: "Connect, Secure and Observe any traffic and Microservices",
-  twitter: "kuma", // @todo update to finalized Twitter handle
+  description: "Build, Secure and Observe your modern Service Mesh",
+  twitter: "KumaMesh", // @todo update to finalized Twitter handle
   author: "Kong",
   repo: "https://github.com/kong/kuma", // @todo update with the finalize repo
   repoButtonLabel: "Star",
