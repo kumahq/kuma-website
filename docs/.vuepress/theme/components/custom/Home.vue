aa9eb393bd84ea8f45b716492ba0b152f9d1f40d
Daryn St. Pierre <daryniscool@gmail.com>
2019-10-17 16:25:16 -0400

    feat/homepage tabs revision (#55)
    * feat: tab style revisions with icon additions based on the new mockup.
    
    * added a fixed width to the tab icons to ensure they never deviate in size.
    
    * chore: cleaned up the tab icon SVG markup
    
    * added a hover state to the tabs. slight content revision to the tabs themselves so that i can properly target their titles in the CSS
    
    * hover and active state revisions for the tabs.
    
    * removed the soft background and icon transitions on the tab states
    
    * fix: normalized the width and text handling for the code blocks within homepage tab content

--- docs/.vuepress/theme/components/custom/Home.vue
+++ docs/.vuepress/theme/components/custom/Home.vue
@@ -1,42 +1,28 @@
 <template>
   <div class="page-container page-container--home" aria-labelledby="masthead-main-title">
-    
     <div class="page-masthead-wrap">
-
       <div class="inner flex flex-wrap -mx-4">
-
         <div class="page-masthead w-full lg:w-1/2 px-4">
           <header class="page-masthead__header">
-            <Content slot-key="masthead-main-title"/>
-            <Content slot-key="masthead-sub-title"/>
+            <Content slot-key="masthead-main-title" />
+            <Content slot-key="masthead-sub-title" />
           </header>
           <div class="page-masthead__actions">
-            <router-link
-              to="/install/" 
-              class="btn btn--bright">
-              Install
-            </router-link>
-            <router-link
-              to="#why-kuma" 
-              class="btn btn--hollow">
-              Why {{ getSiteData.title }}?
-            </router-link>
+            <router-link to="/install/" class="btn btn--bright">Install</router-link>
+            <router-link to="#why-kuma" class="btn btn--hollow">Why {{ getSiteData.title }}?</router-link>
           </div>
         </div>
         <!-- .page-masthead -->
 
         <div class="page-masthead__diagram w-full lg:w-1/2 px-4">
-          <Content slot-key="masthead-diagram"/>
+          <Content slot-key="masthead-diagram" />
         </div>
-
       </div>
       <!-- .inner -->
-    
     </div>
     <!-- .page-masthead-wrap -->
 
     <div class="product-features-wrap">
-
       <!-- <div v-if="$page.frontmatter.newsLink && $page.frontmatter.newsTagline" class="inner newsbar-wrap">
         <div class="newsbar">
           <span class="newsbar__tag">News</span>
@@ -44,85 +30,98 @@
             <a :href="$page.frontmatter.newsLink"> {{$page.frontmatter.newsTagline}} <span class="newsbar__arrow">&rarr;</span></a>
           </p>
         </div>
-      </div> -->
+      </div>-->
 
       <div class="inner product-features flex flex-wrap -mx-4">
-        <Content slot-key="feature-block-content-1" class="product-features__item w-full md:w-1/3 px-4"/>
-        <Content slot-key="feature-block-content-2" class="product-features__item w-full md:w-1/3 px-4"/>
-        <Content slot-key="feature-block-content-3" class="product-features__item w-full md:w-1/3 px-4"/>
+        <Content
+          slot-key="feature-block-content-1"
+          class="product-features__item w-full md:w-1/3 px-4"
+        />
+        <Content
+          slot-key="feature-block-content-2"
+          class="product-features__item w-full md:w-1/3 px-4"
+        />
+        <Content
+          slot-key="feature-block-content-3"
+          class="product-features__item w-full md:w-1/3 px-4"
+        />
       </div>
       <!-- .inner -->
-
     </div>
     <!-- .features-wrap -->
 
     <div v-if="$page.frontmatter.hideTestimonial" class="testimonial-wrap">
-
       <div class="inner testimonial">
         <blockquote class="testimonial__content-wrap">
-          <Content slot-key="testimonial-content"/>
+          <Content slot-key="testimonial-content" />
           <div class="testimonial__cite">
             <div v-if="$page.frontmatter.testimonialPortraitSrc" class="testimonial__portrait">
-              <img :src="$page.frontmatter.testimonialPortraitSrc" :alt="$page.frontmatter.testimonialPortraitAlt">
-            </div> 
+              <img
+                :src="$page.frontmatter.testimonialPortraitSrc"
+                :alt="$page.frontmatter.testimonialPortraitAlt"
+              />
+            </div>
             <cite>
-              <Content slot-key="testimonial-author"/>
-              <Content slot-key="testimonial-author-info"/>
+              <Content slot-key="testimonial-author" />
+              <Content slot-key="testimonial-author-info" />
             </cite>
           </div>
         </blockquote>
       </div>
       <!-- .inner -->
-
     </div>
     <!-- .testimonial-wrap -->
 
+    <div class="steps-wrap-pre-header">
+      <div class="inner">
+        <Content slot-key="tabs-section-title" />
+      </div>
+    </div>
+
     <div class="steps-wrap steps-wrap--slim">
+      <div class="tabs-wrap">
+        <!-- <header class="section-header steps-items__header text-center">
+          <Content slot-key="tabs-section-title" />
+        </header>-->
 
-      <div class="inner tabs-wrap">
-        <header class="section-header steps-items__header text-center">
-          <Content slot-key="tabs-section-title"/>
-        </header>
-
-        <TinyTabs
-          id="get-started-steps-tabs"
-          :anchor="false"
-          :closable="false"
-          :hideTitle="true"
-        >
+        <TinyTabs id="get-started-steps-tabs" :anchor="false" :closable="false" :hideTitle="true">
           <div class="section" id="tab-1">
-            <Content slot-key="tab-1-title" class="title"/>
-            <ol class="steps-items__content steps-items__content--narrow">
-              <li>
-                <Content slot-key="tab-1-content-step-1"/>
-                <Content slot-key="tab-1-code-block-step-1"/>
-              </li>
-              <li>
-                <Content slot-key="tab-1-content-step-2"/>
-                <Content slot-key="tab-1-code-block-step-2"/>
-              </li>
-              <li>
-                <Content slot-key="tab-1-content-step-3"/>
-                <Content slot-key="tab-1-code-block-step-3"/>
-              </li>
-            </ol>
+            <div class="inner">
+              <Content slot-key="tab-1-title" class="title" />
+              <ol class="steps-items__content steps-items__content--narrow">
+                <li>
+                  <Content slot-key="tab-1-content-step-1" />
+                  <Content slot-key="tab-1-code-block-step-1" />
+                </li>
+                <li>
+                  <Content slot-key="tab-1-content-step-2" />
+                  <Content slot-key="tab-1-code-block-step-2" />
+                </li>
+                <li>
+                  <Content slot-key="tab-1-content-step-3" />
+                  <Content slot-key="tab-1-code-block-step-3" />
+                </li>
+              </ol>
+            </div>
           </div>
           <div class="section" id="tab-2">
-            <Content slot-key="tab-2-title" class="title"/>
-            <ol class="steps-items__content steps-items__content--narrow">
-              <li>
-                <Content slot-key="tab-2-content-step-1"/>
-                <Content slot-key="tab-2-code-block-step-1"/>
-              </li>
-              <li>
-                <Content slot-key="tab-2-content-step-2"/>
-                <Content slot-key="tab-2-code-block-step-2"/>
-              </li>
-              <li>
-                <Content slot-key="tab-2-content-step-3"/>
-                <Content slot-key="tab-2-code-block-step-3"/>
-              </li>
-            </ol>
+            <div class="inner">
+              <Content slot-key="tab-2-title" class="title" />
+              <ol class="steps-items__content steps-items__content--narrow">
+                <li>
+                  <Content slot-key="tab-2-content-step-1" />
+                  <Content slot-key="tab-2-code-block-step-1" />
+                </li>
+                <li>
+                  <Content slot-key="tab-2-content-step-2" />
+                  <Content slot-key="tab-2-code-block-step-2" />
+                </li>
+                <li>
+                  <Content slot-key="tab-2-content-step-3" />
+                  <Content slot-key="tab-2-code-block-step-3" />
+                </li>
+              </ol>
+            </div>
           </div>
         </TinyTabs>
       </div>
@@ -147,57 +146,51 @@
             <Content slot-key="step-3-code-block"/>
           </li>
         </ol>
-      </div> -->
+      </div>-->
       <!-- .steps-items -->
 
       <div id="why-kuma">
         <!-- Why Kuma anchor -->
       </div>
-
     </div>
     <!-- .steps-wrap -->
 
     <div class="before-after-wrap">
-
       <div class="inner before-after">
         <header class="section-header">
-          <Content slot-key="before-after-title" class="alt-title"/>
+          <Content slot-key="before-after-title" class="alt-title" />
         </header>
         <div class="flex flex-wrap -mx-12">
           <div class="w-full px-12 md:w-1/2">
             <h4>Before</h4>
-            <Content slot-key="before-after-diagram-1"/>
+            <Content slot-key="before-after-diagram-1" />
           </div>
           <div class="w-full px-12 md:w-1/2">
             <h4>After</h4>
-            <Content slot-key="before-after-diagram-2"/>
+            <Content slot-key="before-after-diagram-2" />
           </div>
         </div>
       </div>
       <!-- .inner -->
-
     </div>
     <!-- .before-after-wrap -->
 
     <div class="newsletter-form-wrap">
-
       <div class="inner newsletter-form">
         <header class="section-header">
-          <Content slot-key="newsletter-title" class="alt-title"/>
+          <Content slot-key="newsletter-title" class="alt-title" />
         </header>
-        <Content slot-key="newsletter-content"/>
+        <Content slot-key="newsletter-content" />
         <NewsletterForm />
       </div>
-
     </div>
     <!-- newsletter-form-wrap -->
-    
   </div>
 </template>
 
 <script>
-import Navbar from '@theme/components/Navbar'
-import TinyTabs from '@theme/components/custom/TinyTabs/VueTinyTabs'
+import Navbar from "@theme/components/Navbar";
+import TinyTabs from "@theme/components/custom/TinyTabs/VueTinyTabs";
 
 export default {
   components: {
--- docs/README.md
+++ docs/README.md
@@ -21,11 +21,15 @@ newsLink: https://konghq.com/kong-summit
 <!-- page masthead -->
 
 ::: slot masthead-main-title
+
 # Build, Secure and Observe<br> your modern Service Mesh
+
 :::
 
 ::: slot masthead-sub-title
+
 ## The open-source control plane for your Service Mesh, delivering high performance and reliability.
+
 :::
 
 ::: slot masthead-diagram
@@ -35,21 +39,27 @@ newsLink: https://konghq.com/kong-summit
 <!-- feature blocks -->
 
 ::: slot feature-block-content-1
+
 ### Universal Control Plane
+
 ![Universal Control Plane diagram](/images/diagrams/diagram-universal-cp@2x.jpg)
 
 Built on top of Envoy, Kuma is a modern control plane to orchestrate L4/L7 traffic, including Microservices and Service Mesh.
 :::
 
 ::: slot feature-block-content-2
+
 ### Powerful Policies
+
 ![Universal Control Plane diagram](/images/diagrams/diagram-powerful-policies@2x.jpg)
 
 Out of the box Ingress and Service Mesh service management policies for security, observability, routing, and more.
 :::
 
 ::: slot feature-block-content-3
+
 ### Platform Agnostic
+
 ![Platform Agnostic diagram](/images/diagrams/diagram-platform-agnostic@2x.jpg)
 
 Enterprise-ready and platform agnostic with native Kubernetes + CRD support, as well as VM and Bare Metal via YAML + REST.
@@ -57,8 +67,8 @@ Enterprise-ready and platform agnostic with native Kubernetes + CRD support, as 
 
 <!-- testimonial -->
 
-::: slot testimonial-content 
-Kuma brings Kong's proven enterprise developer focus to an Envoy based service mesh, which will make it faster and easier 
+::: slot testimonial-content
+Kuma brings Kong's proven enterprise developer focus to an Envoy based service mesh, which will make it faster and easier
 for companies to create and manage cloud-native applications.
 :::
 
@@ -71,42 +81,60 @@ Envoy Proxy Creator, Engineer at Lyft
 :::
 
 <!-- tabs -->
+
 ::: slot tabs-section-title
+
 ## Get Started In 1 Minute
+
 :::
 
 ::: slot tab-1-title
-Kubernetes
+<img class="tab-icon" src="/images/tab-icons/tab-icon-k8s.png" alt="Kubernetes Logo Tab Icon">
+<img class="tab-icon tab-icon__active" src="/images/tab-icons/tab-icon-k8s-active.png" alt="Kubernetes Logo Active Tab Icon">
+
+### Kubernetes
+
 :::
 
 ::: slot tab-1-content-step-1
+
 ### Start the Control Plane
+
 After [downloading and installing Kuma](/install/latest), you can start the control plane. Kuma automatically creates a `default` [Mesh](/docs/latest/policies/#mesh):
 :::
 
 ::: slot tab-1-code-block-step-1
+
 ```sh
 $ kumactl install control-plane | kubectl apply -f -
 ```
+
 :::
 
 ::: slot tab-1-content-step-2
+
 ### Deploy your Services
+
 You can now deploy your services, which will be automatically injected with a Kuma sidecar data-plane:
 :::
 
 ::: slot tab-1-code-block-step-2
+
 ```sh
 $ kubectl apply -f https://raw.githubusercontent.com/Kong/kuma/master/examples/kubernetes/sample-service.yaml
 ```
+
 :::
 
 ::: slot tab-1-content-step-3
+
 ### Apply Policies
+
 You can now apply [Policies](/docs/latest/policies) like Mutual TLS to encrypt the communication within the Mesh. Congratulations! You have secured your Service Mesh!
 :::
 
 ::: slot tab-1-code-block-step-3
+
 ```sh
 $ echo "apiVersion: kuma.io/v1alpha1
 kind: Mesh
@@ -119,29 +147,41 @@ spec:
     ca:
       builtin: {}" | kubectl apply -f -
 ```
+
 :::
 
 ::: slot tab-2-title
-Universal
+<img class="tab-icon" src="/images/tab-icons/tab-icon-uni.png" alt="Universal Logo Tab Icon">
+<img class="tab-icon tab-icon__active" src="/images/tab-icons/tab-icon-uni-active.png" alt="Universal Logo Active Tab Icon">
+
+### Universal
+
 :::
 
 ::: slot tab-2-content-step-1
+
 ### Start the Control Plane
+
 After [downloading and installing Kuma](/install/latest), you can start the control plane. Kuma automatically creates a `default` [Mesh](/docs/latest/policies/#mesh):
 :::
 
 ::: slot tab-2-code-block-step-1
+
 ```sh
 $ kuma-cp run &
 ```
+
 :::
 
 ::: slot tab-2-content-step-2
+
 ### Start your Services and start the data-plane
+
 For each Service that belongs to the Service Mesh, you must start a [`Dataplane Entity`](/docs/latest/documentation/#dataplane-entity). After configuring the networking, you can start the data-plane process:
 :::
 
 ::: slot tab-2-code-block-step-2
+
 ```sh
 $ kuma-tcp-echo --port 9000 # This is a sample service
 
@@ -159,72 +199,92 @@ $ KUMA_CONTROL_PLANE_BOOTSTRAP_SERVER_URL=http://127.0.0.1:5682 \
   KUMA_DATAPLANE_NAME=dp-echo-1 \
   kuma-dp run
 ```
+
 :::
 
 ::: slot tab-2-content-step-3
+
 ### Apply Policies
+
 You can now apply [Policies](/docs/latest/policies) like Mutual TLS to encrypt the communication within the Mesh. Congratulations! You have secured your Service Mesh!
 :::
 
 ::: slot tab-2-code-block-step-3
+
 ```sh
 $ echo "type: Mesh
 name: default
 mtls:
-  enabled: true 
+  enabled: true
   ca:
     builtin: {}" | kumactl apply -f -
 ```
-:::
 
+:::
 
 <!-- steps -->
 
 ::: slot steps-title
+
 ## Build your Service Mesh in 3 steps
+
 :::
 
 ::: slot step-1-content
+
 ### Download and Install Kuma CP
+
 To get started you can download Kuma and install it using the Kuma CLI application: &#96;kumactl&#96;.
 :::
 
 ::: slot step-1-code-block
+
 ```
 $ kumactl install control-plane | kubectl apply -f
 ```
+
 :::
 
 ::: slot step-2-content
+
 ### Install the sidecar Envoy DP
-Once Kuma is up and running, it's now time to install the Envoy sidecars - that Kuma will 
+
+Once Kuma is up and running, it's now time to install the Envoy sidecars - that Kuma will
 later orchestrate - next to any service we want to include into our Service Mesh.
 :::
 
 ::: slot step-2-code-block
+
 ```
 $ kumactl install data-plane | kubectl apply -f
 ```
+
 :::
 
 ::: slot step-3-content
+
 ### Apply Policies
-Congratulations, your Service Mesh is up and running. We can now instruct Kuma to enhance our 
+
+Congratulations, your Service Mesh is up and running. We can now instruct Kuma to enhance our
 Service Mesh with powerful policies like mTLS.
 :::
 
 ::: slot step-3-code-block
+
 ```
 $ kumactl create policy \
   --name mtls \
   --conf topology=hybrid
 ```
+
 :::
 
 <!-- before and after -->
 
 ::: slot before-after-title
+
 ## Run Services, Not Networks
+
 :::
 
 ::: slot before-after-diagram-1
@@ -238,7 +298,9 @@ $ kumactl create policy \
 <!-- newsletter -->
 
 ::: slot newsletter-title
+
 ## Get Community Updates
+
 :::
 
 ::: slot newsletter-content
