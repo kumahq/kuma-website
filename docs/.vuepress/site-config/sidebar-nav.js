5b55945c2c80c31dea530502253f6d322d6a03bd
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-10-25 09:59:04 -0400

    added a draft docs folder for testing with. the script now functions 100% within the website framework. all scripts moved to the bin folder and rewired accordingly for organization purposes.

--- docs/.vuepress/site-config/sidebar-nav.js
+++ docs/.vuepress/site-config/sidebar-nav.js
@@ -1,50 +1,8 @@
-/**
- * Sidebar navigation structure
- *
- * Always include a trailing slash but
- * avoid a slash at the front of the
- * directory.
- *
- * Correct: 'getting-started/'
- * Incorrect: '/getting-started/'
- *
- */
-
 module.exports = {
-  "/docs/0.1.0/": [
-    "",
-    "documentation/",
-    "policies/",
-    "other/"
-  ],
-  "/docs/0.1.1/": [
-    "",
-    "documentation/",
-    "policies/",
-    "other/"
-  ],
-  "/docs/0.1.2/": [
-    "",
-    "documentation/",
-    "policies/",
-    "other/"
-  ],
-  "/docs/0.2.0/": [
-    "",
-    "documentation/",
-    "policies/",
-    "other/"
-  ],
-  "/docs/0.2.1/": [
-    "",
-    "documentation/",
-    "policies/",
-    "other/"
-  ],
-  "/docs/0.2.2/": [
-    "",
-    "documentation/",
-    "policies/",
-    "other/"
-  ],
+  "/docs/0.1.0/": ["", "documentation/", "policies/", "other/"],
+  "/docs/0.1.1/": ["", "documentation/", "policies/", "other/"],
+  "/docs/0.1.2/": ["", "documentation/", "policies/", "other/"],
+  "/docs/0.2.0/": ["", "documentation/", "policies/", "other/"],
+  "/docs/0.2.1/": ["", "documentation/", "policies/", "other/"],
+  "/docs/0.2.2/": ["", "documentation/", "policies/", "other/"]
 };
