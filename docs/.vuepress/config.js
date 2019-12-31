60754c1da574dd2b0b009a343514c02d6cc61146
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2019-12-31 10:08:48 -0500

    Chore/disable service worker (#92)
    * initial script for unregistering all present service workers.
    
    * disabled the service worker in the VuePress PWA configuration. additional script added for unregistering all present service workers.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -110,6 +110,13 @@ module.exports = {
         src: "/preloadPublicAssets.js",
         defer: "defer"
       }
+    ],
+    [
+      "script",
+      {
+        charset: "utf8",
+        src: "/unregisterServiceWorkers.js"
+      }
     ]
   ],
   // version release navigation
@@ -149,8 +156,8 @@ module.exports = {
       ga: productData.gaCode
     },
     "@vuepress/plugin-pwa": {
-      serviceWorker: true,
-      updatePopup: true,
+      serviceWorker: false,
+      updatePopup: false,
       generateSWConfig: {
         skipWaiting: true
       }
