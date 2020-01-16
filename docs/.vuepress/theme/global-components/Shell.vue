ed7959154f68a1c9a68c91d5384a2f9509046b69
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2020-01-16 11:04:06 -0500

    refactored and simplified the main layout component. added a reusable shell component for easier page handling.

--- docs/.vuepress/theme/global-components/Shell.vue
+++ docs/.vuepress/theme/global-components/Shell.vue
@@ -1,37 +1,138 @@
 <template>
   <div
     class="theme-container"
-    @touchstart="$emit('touchStart')"
-    @touchend="$emit('touchEnd')"
+    :class="pageClasses"
+    @touchstart="onTouchStart"
+    @touchend="onTouchEnd"
   >
     <Navbar
-      v-if="showNavbar"
-      @toggle-sidebar="$emit('toggleSidebar')"
+      v-if="shouldShowNavbar"
+      @toggle-sidebar="toggleSidebar"
     />
+
     <div class="content-wrapper">
-      <slot name="page-content"></slot>
+
+      <!-- <div
+        class="sidebar-mask"
+        @click="toggleSidebar(false)"
+      ></div> -->
+
+      <Sidebar
+        :items="sidebarItems"
+        @toggle-sidebar="toggleSidebar"
+      >
+        <slot
+          name="sidebar-top"
+          slot="top"
+        />
+        <slot
+          name="sidebar-bottom"
+          slot="bottom"
+        />
+      </Sidebar>
+
+      <slot name="page-content"/>
+
     </div>
     <!-- .content-wrapper -->
+
     <Footer />
+
   </div>
 </template>
 
 <script>
 import Navbar from '@theme/components/Navbar.vue'
+import Sidebar from '@theme/components/Sidebar.vue'
 
 export default {
+  components: {
+    Sidebar,
+    Navbar
+  },
+
   props: {
-    showNavbar: {
-      type: Boolean,
-      default: true
+    sidebarItems: {
+      type: Array
     }
   },
-  components: {
-    Navbar
+
+  data () {
+    return {
+      isSidebarOpen: false
+    }
+  },
+
+  computed: {
+    shouldShowNavbar () {
+      const { themeConfig } = this.$site
+      const { frontmatter } = this.$page
+      if (
+        frontmatter.navbar === false
+        || themeConfig.navbar === false) {
+        return false
+      }
+      return (
+        this.$title
+        || themeConfig.logo
+        || themeConfig.repo
+        || themeConfig.nav
+        || this.$themeLocaleConfig.nav
+      )
+    },
+
+    shouldShowSidebar () {
+      const { frontmatter } = this.$page
+      return (
+        !frontmatter.home
+        && frontmatter.sidebar !== false
+        && this.sidebarItems.length
+      )
+    },
+
+    pageClasses () {
+      const userPageClass = this.$page.frontmatter.pageClass
+      return [
+        {
+          'no-navbar': !this.shouldShowNavbar,
+          'sidebar-open': this.isSidebarOpen,
+          'no-sidebar': !this.shouldShowSidebar
+        },
+        userPageClass
+      ]
+    }
+  },
+
+  mounted () {
+    this.$router.afterEach(() => {
+      this.isSidebarOpen = false
+    })
+  },
+
+  methods: {
+    toggleSidebar (to) {
+      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
+    },
+
+    // side swipe
+    onTouchStart (e) {
+      this.touchStart = {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY
+      }
+    },
+
+    onTouchEnd (e) {
+      const dx = e.changedTouches[0].clientX - this.touchStart.x
+      const dy = e.changedTouches[0].clientY - this.touchStart.y
+      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+        if (dx > 0 && this.touchStart.x <= 80) {
+          this.toggleSidebar(true)
+        } else {
+          this.toggleSidebar(false)
+        }
+      }
+    }
   }
 }
 </script>
-
-<style>
-
-</style>
--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,31 +1,6 @@
 <template>
-  <Shell
-    :class="pageClasses"
-    showNavbar="shouldShowNavbar"
-    @touchstart="onTouchStart"
-    @touchend="onTouchEnd"
-    @toggle-sidebar="toggleSidebar"
-  >
+  <Shell :sidebarItems="sidebarItems">
     <template slot="page-content">
-      <!-- <div
-        class="sidebar-mask"
-        @click="toggleSidebar(false)"
-      ></div> -->
-
-      <Sidebar
-        :items="sidebarItems"
-        @toggle-sidebar="toggleSidebar"
-      >
-        <slot
-          name="sidebar-top"
-          slot="top"
-        />
-        <slot
-          name="sidebar-bottom"
-          slot="bottom"
-        />
-      </Sidebar>
-
       <component
         :is="layoutComponentSelector"
         :sidebar-items="sidebarItems"
@@ -35,10 +10,8 @@
 </template>
 
 <script>
-import axios from 'axios'
 import Shell from '@theme/global-components/Shell.vue'
 import Page from '@theme/components/Page.vue'
-import Sidebar from '@theme/components/Sidebar.vue'
 import { resolveSidebarItems, redirectToLatestVersion } from '../util'
 
 // specific page templates
@@ -50,21 +23,14 @@ import UseCases from '@theme/components/custom/UseCases.vue'
 
 export default {
   components: {
+    Shell,
     Page,
-    Sidebar,
     Home,
     Install,
     Community,
     RequestADemo,
     UseCases
   },
-
-  data () {
-    return {
-      isSidebarOpen: false
-    }
-  },
-
   computed: {
     layoutComponentSelector() {
 
@@ -89,32 +55,6 @@ export default {
       return layoutComponent
     },
 
-    shouldShowNavbar () {
-      const { themeConfig } = this.$site
-      const { frontmatter } = this.$page
-      if (
-        frontmatter.navbar === false
-        || themeConfig.navbar === false) {
-        return false
-      }
-      return (
-        this.$title
-        || themeConfig.logo
-        || themeConfig.repo
-        || themeConfig.nav
-        || this.$themeLocaleConfig.nav
-      )
-    },
-
-    shouldShowSidebar () {
-      const { frontmatter } = this.$page
-      return (
-        !frontmatter.home
-        && frontmatter.sidebar !== false
-        && this.sidebarItems.length
-      )
-    },
-
     sidebarItems () {
       return resolveSidebarItems(
         this.$page,
@@ -123,50 +63,10 @@ export default {
         this.$localePath
       )
     },
-
-    pageClasses () {
-      const userPageClass = this.$page.frontmatter.pageClass
-      return [
-        {
-          'no-navbar': !this.shouldShowNavbar,
-          'sidebar-open': this.isSidebarOpen,
-          'no-sidebar': !this.shouldShowSidebar
-        },
-        userPageClass
-      ]
-    }
-  },
-
-  mounted () {
-    this.$router.afterEach(() => {
-      this.isSidebarOpen = false
-    })
-  },
-
-  methods: {
-    toggleSidebar (to) {
-      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-    },
-
-    // side swipe
-    onTouchStart (e) {
-      this.touchStart = {
-        x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
-      }
-    },
-
-    onTouchEnd (e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x
-      const dy = e.changedTouches[0].clientY - this.touchStart.y
-      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-        if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true)
-        } else {
-          this.toggleSidebar(false)
-        }
-      }
-    }
   }
 }
 </script>
+
+<style>
+
+</style>
