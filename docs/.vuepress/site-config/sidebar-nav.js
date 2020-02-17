0cd07e5e1a0b8710116c0cb00b1f30e427824f73
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2020-02-17 15:24:59 -0800

    doc pages split up and sidebar revised for v0.3.1.

--- docs/.vuepress/site-config/sidebar-nav.js
+++ docs/.vuepress/site-config/sidebar-nav.js
@@ -50,10 +50,82 @@ module.exports = {
     "other/"
   ],
   "/docs/0.3.1/": [
-    "",
-    "documentation/",
-    "policies/",
-    "other/"
+    {
+      title: "Overview",
+      collapsable: true,
+      sidebarDepth: sidebarDepth,
+      path: "",
+      children: [
+        "overview/what-is-kuma",
+        "overview/why-kuma",
+        "overview/kuma-vs-xyz",
+        "overview/vm-and-k8s-support",
+        "overview/quickstart"
+      ]
+    },
+    {
+      title: "Documentation",
+      collapsable: true,
+      sidebarDepth: sidebarDepth,
+      path: "",
+      children: [
+        "documentation/overview",
+        "documentation/backends",
+        "documentation/dependencies",
+        "documentation/dps-and-data-model",
+        "documentation/cli",
+        "documentation/kumactl",
+        "documentation/gui",
+        "documentation/http-api",
+        "documentation/security",
+        "documentation/ports"
+      ]
+    },
+    {
+      title: "Policies",
+      collapsable: true,
+      sidebarDepth: sidebarDepth,
+      path: "",
+      children: [
+        "policies/introduction",
+        "policies/applying-policies",
+        "policies/mesh",
+        "policies/mutual-tls",
+        "policies/traffic-permissions",
+        "policies/traffic-route",
+        "policies/traffic-tracing",
+        "policies/traffic-log",
+        "policies/health-check",
+        "policies/proxy-template",
+        "policies/general-notes-about-kuma-policies",
+        "policies/how-kuma-chooses-the-right-policy-to-apply"
+      ]
+    },
+    {
+      title: "Install",
+      collapsable: true,
+      sidebarDepth: sidebarDepth,
+      path: "",
+      children: [
+        "installation/centos",
+        "installation/debian",
+        "installation/kubernetes",
+        "installation/macos",
+        "installation/redhat",
+        "installation/ubuntu",
+      ]
+    },
+    {
+      title: "Other",
+      collapsable: true,
+      sidebarDepth: sidebarDepth,
+      path: "",
+      children: [
+        "other/introduction",
+        "other/enterprise",
+        "other/license"
+      ]
+    },
   ],
   "/docs/0.3.2/": [
     {
--- docs/docs/0.3.1/other/enterprise.md
+++ docs/docs/0.3.1/other/enterprise.md
@@ -1,8 +1,4 @@
-# Other
-
-Here you will find other content related to Kuma. Feel free to [get in touch](/community) with the community if you wish more content or examples to be added to the official documentation.
-
-## Enterprise
+# Enterprise
 
 Kuma has been created at [Kong](https://konghq.com) from the experience and feedback of 150+ enterprise organizations implementing Service Mesh and Microservices within both new and existing architectures.
 
@@ -11,11 +7,3 @@ Kong is and always has been a proud contributor of open-source software. That kn
 We also understand how critical and how important it is to have a dedicated relationship for support SLAs, implementation and customization, therefore we offer an Enterprise package that can be explored for mission-critical deployments. This package integrates with the Kong Service Control Platform to provide a full-cycle API and Microservices management platform for the entire organization.
 
 You can explore the Enterprise offering by [requesting a demo](/enterprise).
-
-## License
-
-Kuma is an open-source product that can be downloaded and used freely in accordance to the [Apache License Version 2.0](https://github.com/Kong/kuma/blob/master/LICENSE).
-
-Community is at the DNA of Kuma. It is what makes Kuma better every day by either contributing or using the product.
-
-You can learn more about Kuma's community from the [Community Page](/community).
