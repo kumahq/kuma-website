de4973f1c45f05bf066afbe647938189201ebc01
Marco Palladino <marco@konghq.com>
2020-05-12 15:53:58 -0700

    release: 0.5.0 (#195)
    * docs(documentation) fine tuning of snapshot generation
    
    * feat: adding installer script
    
    * feat(docs) vuepress-plugin-tabs plugin added and activated. subtle style overrides to the tabs added. (#165)
    
    * docs: updating mTLS output with new format
    
    * docs: kumactl install tracing
    
    * docs(kuma-cp) Fault Injection (#168)
    
    * docs(http-api) pagination (#179)
    
    * Add Fault Injections to the policies page (#172)
    
    * replaced any doc images that have the old logo in them. switched the homepage images out to use images with the new logo. updated the main logo in the masthead.
    
    * chore: adding automatic installer script and revamping docs (#164)
    
    Including installation + quickstart.
    
    * docs(install): minor updates
    
    * updated the image manifest.
    
    * updated all social images and favicons with the new logo.
    
    * fix(installer): checking for gzip and better support for amazon linux ami (#189)
    
    * docs: removing kuma-injector references (#188)
    
    * chore(docs) restored the doc images for versions 0.1.0 through 0.2.0. versions 0.3.2 and 0.4.0 don't contain any branded diagrams that are used in the docs, so there was nothing to restore there.
    
    * chore(docs) updated all images in the 0.4.0 docs to use diagrams from a 0.4.0 image folder, instead of using diagrams from the 0.2.0 folder.
    
    * chore(docs) copied over other remaining images from the 0.3.2 folder that were used in 0.4.0 docs, so that all images referenced in the 0.4.0 docs pull from a 0.4.0 image folder in order to reduce confusion and make further updates easier.
    
    * chore(docs) added a 0.5.0 folder for doc images and added the new diagrams to it for when we are ready to cut a new version.
    
    * chore(site) changed the cache busting string into a randomly-generated string that changes each time the site is built to ensure all favicons and touch icons are refreshed in browsers. auto-generated the new image manifest.
    
    * docs(install): homebrew support (#185)
    
    * chore(docs) updated the 0.4.0 doc diagram image paths so they're linked to the ones in the 0.5.0 image folder.
    
    * new mesh format (#181)
    
    Co-authored-by: Marco Palladino <marco@konghq.com>
    
    * docs(http-api) creation and modification time (#192)
    
    Co-authored-by: Marco Palladino <marco@konghq.com>
    
    * docs(policies) log external services (#193)
    
    * Release/0.5.0 (#194)
    
    * removing yarn.lock
    
    * adding yarn.lock
    
    Co-authored-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
    Co-authored-by: Daryn St. Pierre <daryniscool@gmail.com>
    Co-authored-by: Ilya Lobkov <lobkovilya@yandex.ru>
    Co-authored-by: Daryn St. Pierre <daryn.st.pierre@konghq.com>

---
+++ docs/.vuepress/public/installer.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+
+# Copyright 2019-2020 Kong Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# You can customize the version of Kuma to download by setting the
+# KUMA_VERSION environment variable, and you can change the default 64bit
+# architecture by setting the KUMA_ARCH variable.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+: "${KUMA_VERSION:=}"
+: "${KUMA_ARCH:=amd64}"
+
+DISTRO=""
+
+printf "\n"
+printf "INFO\tWelcome to the Kuma automated download!\n"
+
+if ! type "grep" > /dev/null 2>&1; then
+  printf "ERROR\tgrep cannot be found\n"
+  exit 1;
+fi
+if ! type "curl" > /dev/null 2>&1; then
+  printf "ERROR\tcurl cannot be found\n"
+  exit 1;
+fi
+if ! type "tar" > /dev/null 2>&1; then
+  printf "ERROR\ttar cannot be found\n"
+  exit 1;
+fi
+if ! type "gzip" > /dev/null 2>&1; then
+  printf "ERROR\tgzip cannot be found\n"
+  exit 1;
+fi
+
+OS=`uname -s`
+if [ "$OS" = "Linux" ]; then
+  DISTRO=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
+  if [ "$DISTRO" = "amzn" ]; then
+    DISTRO="centos"
+  fi
+elif [ "$OS" = "Darwin" ]; then
+  DISTRO="darwin"
+else
+  printf "ERROR\tOperating system %s not supported by Kuma\n" "$OS"
+  exit 1
+fi
+
+if [ -z "$DISTRO" ]; then
+  printf "ERROR\tUnable to detect the operating system\n"
+  exit 1
+fi
+
+if [ -z "$KUMA_VERSION" ]; then
+  # Fetching latest Kuma version
+  printf "INFO\tFetching latest Kuma version..\n"
+  KUMA_VERSION=`curl -s https://kuma.io/latest_version`
+  if [ $? -ne 0 ]; then
+    printf "ERROR\tUnable to fetch latest Kuma version.\n"
+    exit 1
+  fi
+  if [ -z "$KUMA_VERSION" ]; then
+    printf "ERROR\tUnable to fetch latest Kuma version because of a problem with Kuma.\n"
+    exit 1
+  fi
+fi
+
+printf "INFO\tKuma version: %s\n" "$KUMA_VERSION"
+printf "INFO\tKuma architecture: %s\n" "$KUMA_ARCH"
+printf "INFO\tOperating system: %s\n" "$DISTRO"
+
+URL="https://kong.bintray.com/kuma/kuma-$KUMA_VERSION-$DISTRO-$KUMA_ARCH.tar.gz"
+
+if ! curl -s --head $URL | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null; then
+  printf "ERROR\tUnable to download Kuma at the following URL: %s\n" "$URL"
+  exit 1
+fi
+
+printf "INFO\tDownloading Kuma from: %s" "$URL"
+printf "\n\n"
+
+if curl -L "$URL" | tar xz; then
+  printf "\n"
+  printf "INFO\tKuma %s has been downloaded!\n" "$KUMA_VERSION"
+  printf "\n"
+  printf "%s" "$(<$DIR/kuma-$KUMA_VERSION/README)"
+  printf "\n"
+else
+  printf "\n"
+  printf "ERROR\tUnable to download Kuma\n"
+  exit 1
+fi
---
+++ docs/docs/0.4.0/installation/openshift.md
@@ -0,0 +1,5 @@
+# OpenShift
+
+Full instructions for Red Hat OpenShift have been added since Kuma version 0.5 and above.
+
+To use OpenShift, [download the latest version](/docs/latest/installation/openshift/) of Kuma.
