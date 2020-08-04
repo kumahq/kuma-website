d12ec56484f27e15151eab5ba0add0f8e7143b03
subnetmarco <88.marco@gmail.com>
2020-08-04 08:35:27 -0700

    chore(footer): adding CNCF footer
    Signed-off-by: subnetmarco <88.marco@gmail.com>

--- docs/.vuepress/theme/global-components/Footer.vue
+++ docs/.vuepress/theme/global-components/Footer.vue
@@ -1,11 +1,14 @@
 <template>
   <footer class="page-footer">
 
-    <!-- <div class="cncf-banner">
-      <h4>{{ getSiteData.title }} is a CNCF incubating project</h4>
+    <div class="cncf-banner">
+      <h4>{{ getSiteData.title }} is a CNCF Sandbox project</h4>
       <img src="/images/CNCF_Alternate_Pantone.png" alt="Cloud Native Computing Foundation Logo">
-    </div> -->
+      <br>
+      <p>Kuma was originally created by <a href="https://konghq.com/">Kong, Inc.</a> and made successful by its open <a href="/community">community</a>.</p>
+    </div>
     
+    <!--
     <div class="page-footer__links">
       <div class="inner flex flex-wrap -mx-2">
         <div v-for="(link, index) in links" :key="index" class="w-full sm:w-1/2 md:w-1/4 px-2">
@@ -20,6 +23,8 @@
         </div>
       </div>
     </div>
+    -->
+    
     <!-- .page-footer__links -->
 
     <div class="page-footer__sublinks">
@@ -77,61 +82,19 @@ import GithubButton from 'vue-github-button'
 export default {
   data() {
     return {
-      copyright: `Kong, Inc. ${new Date().getFullYear()}`,
+      copyright: `Copyright The Kuma Authors.`,
       links: [
         {
-          group: 'Products',
+          group: 'Community',
           items: [
-            { label: 'Kong', url: 'https://konghq.com/' },
-            { label: 'Kong Enterprise', url: 'https://konghq.com/kong-enterprise/' },
-            { label: 'Kong Cloud', url: 'https://konghq.com/cloud/' },
-            { label: 'Subscriptions', url: 'https://konghq.com/subscriptions/' }
-          ]
-        },
-        {
-          group: 'Resources',
-          items: [
-            { label: 'Documentation', url: '/docs/' },
-            { label: 'Ebooks', url: 'https://konghq.com/ebooks/' },
-            { label: 'Briefs', url: 'https://konghq.com/briefs/' },
-            { label: 'Webinars', url: 'https://konghq.com/webinars/' },
-            { label: 'Blog', url: 'https://konghq.com/blog/' },
-            { label: 'Meetups', url: 'https://www.meetup.com/members/121234612/' }
-          ]
-        },
-        {
-          group: 'Company',
-          items: [
-            { label: 'About', url: 'https://konghq.com/about-kong-inc/' },
-            { label: 'Customers', url: 'https://konghq.com/customers/' },
-            { label: 'Investors', url: 'https://konghq.com/investors/' },
-            {
-              label: 'Careers',
-              url: 'https://konghq.com/careers/',
-              badge: true,
-              badgeText: 'Hiring!'
-            },
-            { label: 'Partners', url: 'https://konghq.com/partners/' },
-            { label: 'Press', url: 'https://konghq.com/press-room/' },
-            { label: 'Contact', url: 'https://konghq.com/contact/' },
-            { label: 'Kong Summit', url: 'https://konghq.com/kong-summit/' }
-          ]
-        },
-        {
-          group: 'Open Source',
-          items: [
-            { label: 'Install Kong Gateway', url: 'https://konghq.com/install/' },
-            { label: 'Kong Community', url: 'https://konghq.com/community/' },
-            { label: 'Kubernetes Ingress', url: 'https://github.com/Kong/kubernetes-ingress-controller' },
-            { label: 'Kuma', url: '/' },
-            { label: 'Insomnia', url: 'https://insomnia.rest/' }
+            { label: 'GitHub', url: 'https://github.com/kumahq/kuma' },
+            { label: 'Slack', url: 'https://chat.kuma.io/' },
+            { label: 'Twitter', url: 'https://twitter.com/kumamesh' }
           ]
         }
       ],
       subLinks: [
-        { label: 'Terms', url: '/terms/' },
-        { label: 'Privacy', url: '/privacy/' },
-        // { label: 'Sitemap', url: '#' }
+        { label: 'Code Of Conduct', url: '/terms/' }
       ],
       socialIconPath: '/images/social/',
       socialIconSize: 16, // in px
