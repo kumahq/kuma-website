3165a029d73de0c7eff43704065492b335896732
Daryn St. Pierre <daryn.st.pierre@konghq.com>
2021-02-03 12:12:41 -0500

    GA event functions added to forms. fyi, these only work in production.
    Signed-off-by: Daryn St. Pierre <daryn.st.pierre@konghq.com>

--- docs/.vuepress/theme/global-components/CommunityCallForm.vue
+++ docs/.vuepress/theme/global-components/CommunityCallForm.vue
@@ -144,7 +144,13 @@ export default {
     },
 
     formIsSubmitting() {
+      // change the sending status
       this.formSending = true
+      
+      // push an event to Google Analytics
+      if (process.env.NODE_ENV === 'production') {
+        window.ga('send', 'event', 'Newsletter Form Submission')
+      }
     },
   }
 }
