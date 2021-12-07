c1d0d5ced75ae7321742575a2f3d8996f3e6dbbc
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2021-12-07 23:01:05 +0100

    docs(security) token expiration, revocation, signing key rotation (#616)
    * docs(security) token expiration, revocation, signing key rotation
    
    Signed-off-by: Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
    
    * move files to new next directory
    
    Signed-off-by: Jennifer Rondeau <jennifer.rondeau@konghq.com>
    
    * fix up paths
    
    Signed-off-by: Jennifer Rondeau <jennifer.rondeau@konghq.com>
    
    Co-authored-by: Jennifer Rondeau <jennifer.rondeau@konghq.com>

--- docs/.vuepress/site-config/sidebar-nav.js
+++ docs/.vuepress/site-config/sidebar-nav.js
@@ -2123,6 +2123,8 @@ module.exports = {
         "security/secrets",
         "security/api-access-control",
         "security/api-server-auth",
+        "security/dp-auth",
+        "security/zone-ingress-auth",
       ]
     },
     {
