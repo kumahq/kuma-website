bffd56bd4df1ef60a7bef93c51480ae02d97e501
Bart Smykla <bartek@smykla.com>
2022-06-01 10:28:32 +0200

    refactor: adjust installer.sh to possible reuse (#837)
    Improve the shell scripts according to shellcheck + allow vars
    like `$PRODUCT_NAME`, `$LATEST_VERSION` or `$REPO_PREFIX`
    to be provided in advance.
    
    Set fallback for `$ARCH` variable value to `amd64`
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- docs/.vuepress/public/installer.sh
+++ docs/.vuepress/public/installer.sh
@@ -21,15 +21,14 @@
 DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
 : "${VERSION:=}"
-: "${ARCH:=}"
-
-PRODUCT_NAME=Kuma
-LATEST_VERSION=https://kuma.io/latest_version
-REPO_PREFIX=kuma
-CTL_NAME=kumactl
+: "${ARCH:=amd64}"
+: "${PRODUCT_NAME:=Kuma}"
+: "${LATEST_VERSION:=https://kuma.io/latest_version}"
+: "${REPO_PREFIX:=kuma}"
+: "${CTL_NAME:=kumactl}"
 
 printf "\n"
-printf "INFO\tWelcome to the $PRODUCT_NAME automated download!\n"
+printf "INFO\tWelcome to the %s automated download!\n" "$PRODUCT_NAME"
 
 if ! type "grep" > /dev/null 2>&1; then
   printf "ERROR\tgrep cannot be found\n"
@@ -49,7 +48,7 @@ if ! type "gzip" > /dev/null 2>&1; then
 fi
 
 DISTRO=""
-OS=`uname -s`
+OS=$(uname -s)
 if [ "$OS" = "Linux" ]; then
   DISTRO=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
   if [ "$DISTRO" = "amzn" ]; then
@@ -58,7 +57,7 @@ if [ "$OS" = "Linux" ]; then
 elif [ "$OS" = "Darwin" ]; then
   DISTRO="darwin"
 else
-  printf "ERROR\tOperating system %s not supported by $PRODUCT_NAME\n" "$OS"
+  printf "ERROR\tOperating system %s not supported by %s\n" "$OS" "$PRODUCT_NAME"
   exit 1
 fi
 
@@ -67,28 +66,32 @@ if [ -z "$DISTRO" ]; then
   exit 1
 fi
 
-DETECTED_ARCH=`uname -m`
+DETECTED_ARCH=$(uname -m)
 if [ "$ARCH" = "" ]; then
   if [ "$DETECTED_ARCH" = "x86_64" ]; then
     ARCH=amd64
-  elif [ "$DETECTED_ARCH" = "arm64" ] || [ "$DETECTED_ARCH" = "aarch64" ] || [ "$DETECTED_ARCH" = "armv8l" ] || [ "$DETECTED_ARCH" = "armv8b" ]; then
+  elif [ "$DETECTED_ARCH" = "arm64" ] || \
+    [ "$DETECTED_ARCH" = "arch64" ] || \
+    [ "$DETECTED_ARCH" = "armv8l" ] || \
+    [ "$DETECTED_ARCH" = "armv8b" ]; then
     ARCH=arm64
   else
-    printf "ERROR\tArchitecture %s not supported by $PRODUCT_NAME\n" "$DETECTED_ARCH"
+    printf "ERROR\tArchitecture %s not supported by %s\n" "$DETECTED_ARCH" "$PRODUCT_NAME"
     exit 1
   fi
 fi
 
 if [ -z "$VERSION" ]; then
   # Fetching latest version
-  printf "INFO\tFetching latest $PRODUCT_NAME version..\n"
-  VERSION=`curl -s $LATEST_VERSION`
-  if [ $? -ne 0 ]; then
-    printf "ERROR\tUnable to fetch latest $PRODUCT_NAME version.\n"
+  printf "INFO\tFetching latest %s version..\n" "$PRODUCT_NAME"
+
+  if ! VERSION=$(curl -s $LATEST_VERSION); then
+    printf "ERROR\tUnable to fetch latest %s version.\n" "$PRODUCT_NAME"
     exit 1
   fi
+
   if [ -z "$VERSION" ]; then
-    printf "ERROR\tUnable to fetch latest $PRODUCT_NAME version because of a problem with $PRODUCT_NAME.\n"
+    printf "ERROR\tUnable to fetch latest %s version because of a problem with %s.\n" "$PRODUCT_NAME" "$PRODUCT_NAME"
     exit 1
   fi
 fi
@@ -103,6 +106,7 @@ fi
 URL="https://download.konghq.com/mesh-alpine/$REPO_PREFIX-$VERSION-$DISTRO-$ARCH.tar.gz"
 
 if ! curl -s --head "$URL" | head -n 1 | grep -E 'HTTP/1.1 [23]..|HTTP/2 [23]..' > /dev/null; then
+  # shellcheck disable=SC2034
   IFS=. read -r major minor patch <<EOF
 ${VERSION}
 EOF
@@ -110,32 +114,32 @@ EOF
   # handle the kumactl archive
   if [ "$OS" = "Linux" ]; then
       if  [ "$major" -ge "1" ] && [ "$minor" -ge "7" ]; then
-          printf "INFO\tWe don't compile the $PRODUCT_NAME executables for your Linux distribution.\n"
-          printf "INFO\tFetching $CTL_NAME...\n"
+          printf "INFO\tWe don't compile the %s executables for your Linux distribution.\n" "$PRODUCT_NAME"
+          printf "INFO\tFetching %s...\n" "$CTL_NAME"
           URL="https://download.konghq.com/mesh-alpine/$REPO_PREFIX-$CTL_NAME-$VERSION-linux-$ARCH.tar.gz"
           if ! curl -s --head "$URL" | head -n 1 | grep -E 'HTTP/1.1 [23]..|HTTP/2 [23]..' > /dev/null; then
-            printf "ERROR\tUnable to download $CTL_NAME at the following URL: %s\n" "$URL"
+            printf "ERROR\tUnable to download %s at the following URL: %s\n" "$CTL_NAME" "$URL"
             exit 1
           fi
       else
         printf "WARNING\tYou appear to be running an unsupported Linux distribution.\n"
       fi
   fi
-  printf "ERROR\tUnable to download $PRODUCT_NAME at the following URL: %s\n" "$URL"
+  printf "ERROR\tUnable to download %s at the following URL: %s\n" "$PRODUCT_NAME" "$URL"
   exit 1
 fi
 
-printf "INFO\tDownloading $PRODUCT_NAME from: %s" "$URL"
+printf "INFO\tDownloading %s from: %s" "$PRODUCT_NAME" "$URL"
 printf "\n\n"
 
 if curl -L "$URL" | tar xz; then
   printf "\n"
-  printf "INFO\t$PRODUCT_NAME %s has been downloaded!\n" "$VERSION"
+  printf "INFO\t%s %s has been downloaded!\n" "$PRODUCT_NAME" "$VERSION"
   printf "\n"
-  printf "%s" "$(<$DIR/$REPO_PREFIX-$VERSION/README)"
+  printf "%s" "$(cat "$DIR/$REPO_PREFIX-$VERSION/README")"
   printf "\n"
 else
   printf "\n"
-  printf "ERROR\tUnable to download $PRODUCT_NAME\n"
+  printf "ERROR\tUnable to download %s\n" "$PRODUCT_NAME"
   exit 1
 fi
