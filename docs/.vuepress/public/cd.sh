4f7187e4b2615e84b6365b7d724a8d9526886288
Charly Molter <charly.molter@konghq.com>
2022-07-07 12:21:48 +0200

    feat(cd): add a contribute section and a script to get the latest version
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

---
+++ docs/.vuepress/public/cd.sh
@@ -0,0 +1,41 @@
+: "${BRANCH:=master}"
+: "${REPO:=kumahq/kuma}"
+
+IFS=/ read -r owner repo << EOF
+${REPO}
+EOF
+
+JQCMD='.data.repository.ref.target.history.nodes | map(select(.statusCheckRollup.state == "SUCCESS")) | first | .oid'
+COMMIT=`gh api graphql  -f owner=${owner} -f repo=${repo} -f branch=${BRANCH} --jq "${JQCMD}" -F query='
+query($owner: String!, $repo: String!, $branch: String!) {
+  repository(owner: $owner, name:$repo) {
+    ref(qualifiedName: $branch) {
+        target{
+          ... on Commit {
+          history(first: 10){
+            nodes{
+              oid
+              statusCheckRollup {
+                state
+              }
+            }
+         }
+        }
+       }
+     }
+  }
+}
+'`
+
+echo "Getting release $VERSION"
+
+if [ $REPO == "kong/kong-mesh" ]; then
+  PRODUCT_NAME="KONG MESH"
+  REPO_PREFIX="kong-mesh"
+  COMMIT=${COMMIT:0:7}
+else
+  COMMIT=${COMMIT:0:9}
+fi
+VERSION=0.0.0-preview.${COMMIT}
+
+curl -s https://kuma.io/installer.sh | PRODUCT_NAME=${PRODUCT_NAME} REPO_PREFIX=${REPO_PREFIX} VERSION=${VERSION} sh -
