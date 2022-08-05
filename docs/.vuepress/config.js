ac5b6b0b28a81fa1350d2f293bee5472d4df0c03
Charly Molter <charly.molter@konghq.com>
2022-08-05 09:25:58 +0200

    fix(search): add back searchbar
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -396,7 +396,7 @@ Sitemap: https://kuma.io/sitemap.xml
             if (!v) {
               return
             }
-            const {meta = [], sitemap = {}} = page.frontmatter;
+            let {meta = [], search = false} = page.frontmatter;
             meta.push(
               {
                 name: "docsearch:section",
@@ -408,14 +408,14 @@ Sitemap: https://kuma.io/sitemap.xml
             )
             if (v !== versions.latestMinor) {
               // Only index the latest version of the docs
-              sitemap.exclude = true
+              search = true
               meta.push({
                 name: "robots",
                 content: "noindex,follow"
               });
             }
             page.frontmatter.meta = meta
-            page.frontmatter.search = sitemap
+            page.frontmatter.search = search
 
             let ver = versions.versions(v);
             if (ver) {
