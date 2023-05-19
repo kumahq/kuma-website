157d1bc0007e338cf9bba88a1909e8569a2a16da
Bart Smykla <bartek@smykla.com>
2023-05-19 09:53:17 +0200

    fix(script): fix PRODUCT_NAME default for preview.sh
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- app/preview.sh
+++ app/preview.sh
@@ -1,5 +1,5 @@
 : "${BRANCH:=master}"
 : "${REPO:=kumahq/kuma}"
-: "${PRODUCT_NAME:=KONG MESH}"
+: "${PRODUCT_NAME:=Kuma}"
 
 curl -s https://kuma.io/installer.sh | BRANCH=${BRANCH} REPO=${REPO} PRODUCT_NAME=${PRODUCT_NAME} VERSION=preview sh -
