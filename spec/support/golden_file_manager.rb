be16698ae5b0684bd00b3d4b86ef0340539548a4
slonka <slonka@users.noreply.github.com>
2024-10-23 11:56:29 +0200

    chore(meshservice): added removing of uuid
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- spec/support/golden_file_manager.rb
+++ spec/support/golden_file_manager.rb
@@ -12,19 +12,27 @@ module GoldenFileManager
     File.write(file_path, content)
   end
 
+  def self.remove_dynamic_attributes(html)
+    # Remove all occurrences of data-tab attributes
+    html.gsub(/ data-tab="[^"]*"/, '')
+  end
+
   def self.assert_output(output, golden_path)
+    # Clean up output to remove dynamic attributes
+    cleaned_output = remove_dynamic_attributes(output).strip
+
     if File.exist?(golden_path)
-      golden_content = load_golden(golden_path)
-      if output.strip != golden_content.strip
+      golden_content = load_golden(golden_path).strip
+      if cleaned_output != golden_content
         if ENV['UPDATE_GOLDEN_FILES'] == 'true'
-          update_golden(golden_path, output)
+          update_golden(golden_path, cleaned_output)
           puts "Golden file updated: #{golden_path}"
         else
           raise "Output does not match golden file #{golden_path}."
         end
       end
     else
-      update_golden(golden_path, output)
+      update_golden(golden_path, cleaned_output)
       puts "Golden file created: #{golden_path}"
     end
   end
