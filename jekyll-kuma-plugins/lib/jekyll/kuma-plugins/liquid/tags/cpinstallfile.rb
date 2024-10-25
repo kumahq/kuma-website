d3ada8351d4d94418e236448e4dc34f45d60b2d6
Bart Smykla <bartek@smykla.com>
2024-10-25 15:32:23 +0200

    feat(jekyll): add `cpinstallfile` and `helmvalues` jekyll plugins
    These two plugins are gonna be used in the following up PRs with
    Transparent Proxy documentation overhaul.
    
    `cpinstallfile` will just generate tabs for `kumactl` and Helm when you
    want to specify values through `-f|--values` flags.
    
    `helmvalues` will render a code block with a proper yaml, which will be
    prefixed with a provided or `{{site.set_flag_values_prefix}}` path, so
    for example:
    
    ```
    {% helmvalues %}
    ~~~yaml
    controlPlane:
     config: |
       runtime:
         kubernetes:
           injector:
             sidecarTraffic:
               excludeOutboundIPs: [172.10.0.250, 169.254.169.252]
    ~~~
    {% endhelmvalues %}
    ```
    
    will take into account `{{site.set_flag_values_prefix}}` (in following
    example it will be equal `provided_prefix.`), and render:
    
    ```yaml
    provided_prefix:
      controlPlane:
        config: |
          runtime:
            kubernetes:
              injector:
                sidecarTraffic:
                  excludeOutboundIPs: [172.10.0.250, 169.254.169.252]
    ```
    
    or when it'll be empty
    
    ```yaml
    controlPlane:
      config: |
        runtime:
          kubernetes:
            injector:
              sidecarTraffic:
                excludeOutboundIPs: [172.10.0.250, 169.254.169.252]
    ```
    
    Signed-off-by: Bart Smykla <bartek@smykla.com>

---
+++ jekyll-kuma-plugins/lib/jekyll/kuma-plugins/liquid/tags/cpinstallfile.rb
@@ -0,0 +1,65 @@
+# This plugin simplifies installation documentation by generating tabs for `kumactl`
+# and Helm commands, using a set of customizable parameters. It reduces the need for
+# duplicate installation examples across Universal and Kubernetes environments.
+# The plugin requires a tabs name to be provided, otherwise it will raise an error.
+
+require 'yaml'
+
+module Jekyll
+  module KumaPlugins
+    module Liquid
+      module Tags
+        class CpInstallFile < ::Liquid::Tag
+          def initialize(tag_name, text, tokens)
+            super
+            @tabs_name, *params_list = @markup.split(' ')
+            @params = { "filename" => "values.yaml" }
+
+            if @tabs_name.nil? || @tabs_name.strip.empty?
+              raise ArgumentError, "You must provide a valid tabs name for the cpinstallfile tag."
+            end
+
+            params_list.each do |item|
+              key, value = item.split('=')
+              @params[key] = value.strip.gsub(/^"+|"+$/, '') if value && !value.empty?
+            end
+          end
+
+          def render(context)
+            ::Liquid::Template.parse(content).render(context)
+          end
+
+          private
+
+          def content
+            tabs_name = @tabs_name.strip
+            filename = @params["filename"]
+
+            <<~MARKDOWN
+              {% tabs #{tabs_name} useUrlFragment=false %}
+              {% tab #{tabs_name} kumactl %}
+              ```sh
+              kumactl install control-plane --values #{filename} | kubectl apply -f -
+              ```
+              {% endtab %}
+              {% tab #{tabs_name} Helm %}
+              Before using {{site.mesh_product_name}} with Helm, ensure that you’ve followed [these steps](/docs/{{page.version}}/production/cp-deployment/kubernetes/#helm) to configure your local Helm repository.
+
+              ```sh
+              helm install \\
+                --create-namespace \\
+                --namespace {{site.mesh_namespace}} \\
+                --values #{filename} \\
+                {{site.mesh_helm_install_name}} {{site.mesh_helm_repo}}
+              ```
+              {% endtab %}
+              {% endtabs %}
+            MARKDOWN
+          end
+        end
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('cpinstallfile', Jekyll::KumaPlugins::Liquid::Tags::CpInstallFile)
