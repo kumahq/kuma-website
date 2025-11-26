c507e2e31c73d4d7f5689e56c79af6c0b388dcbb
Marcin Skalski <skalskimarcin33@gmail.com>
2025-11-26 09:08:15 +0100

    revert explicit return (conflicts w/ rubocop)
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- jekyll-kuma-plugins/lib/jekyll/kuma-plugins/liquid/tags/embed.rb
+++ jekyll-kuma-plugins/lib/jekyll/kuma-plugins/liquid/tags/embed.rb
@@ -28,7 +28,7 @@ module Jekyll
               data
             rescue StandardError => e
               Jekyll.logger.warn('Failed reading raw file', e)
-              return nil
+              nil
             end
           end
         end
