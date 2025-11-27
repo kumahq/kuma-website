e912dd02fc905a8fe7c55544417b84757db189cd
Marcin Skalski <skalskimarcin33@gmail.com>
2025-11-27 07:46:33 +0100

    refactor(plugins): extract methods from policyyaml.rb
    - create TerraformGenerator class w/ yaml_to_terraform conversion
    - extract tab generation into dedicated methods
    - extract register_default_transformers from initialize
    - remove TODO comments and rubocop:disable
    
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

---
+++ jekyll-kuma-plugins/lib/jekyll/kuma_plugins/liquid/tags/policyyaml/terraform_generator.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module KumaPlugins
+    module Liquid
+      module Tags
+        # Generates Terraform HCL from YAML policy data
+        class TerraformGenerator
+          def generate(yaml_data)
+            type = yaml_data['type']
+            name = yaml_data['name']
+            resource_name = "konnect_#{snake_case(type)}"
+
+            build_resource(resource_name, name, yaml_data)
+          end
+
+          private
+
+          def build_resource(resource_name, name, yaml_data)
+            terraform = "resource \"#{resource_name}\" \"#{name.gsub('-', '_')}\" {\n"
+            terraform += resource_prefix
+            yaml_data.each do |key, value|
+              next if key == 'mesh'
+
+              terraform += convert_value(key, value, 1)
+            end
+            terraform += resource_suffix
+            terraform += "}\n"
+            terraform
+          end
+
+          def resource_prefix
+            <<-HEREDOC
+  provider = konnect-beta
+            HEREDOC
+          end
+
+          def resource_suffix
+            <<-HEREDOC
+  labels   = {
+    "kuma.io/mesh" = konnect_mesh.my_mesh.name
+  }
+  cp_id    = konnect_mesh_control_plane.my_meshcontrolplane.id
+  mesh     = konnect_mesh.my_mesh.name
+            HEREDOC
+          end
+
+          def convert_value(key, value, indent_level, is_in_array: false, is_last: false)
+            key = snake_case(key) unless key.empty?
+            indent = '  ' * indent_level
+
+            case value
+            when Hash
+              convert_hash(key, value, indent, indent_level, is_in_array, is_last)
+            when Array
+              convert_array(key, value, indent, indent_level, is_in_array, is_last)
+            else
+              convert_scalar(key, value, indent, is_in_array, is_last)
+            end
+          end
+
+          def convert_hash(key, value, indent, indent_level, is_in_array, is_last)
+            result = is_in_array ? "#{indent}{\n" : "#{indent}#{key} = {\n"
+            value.each_with_index do |(k, v), index|
+              result += convert_value(k, v, indent_level + 1, is_last: index == value.size - 1)
+            end
+            result += "#{indent}}#{trailing_comma(is_in_array, is_last)}\n"
+          end
+
+          def convert_array(key, value, indent, indent_level, is_in_array, is_last)
+            result = "#{indent}#{key} = [\n"
+            value.each_with_index do |v, index|
+              is_last_item = index == value.size - 1
+              result += convert_value('', v, indent_level + 1, is_in_array: true, is_last: is_last_item)
+            end
+            result += "#{indent}]#{trailing_comma(is_in_array, is_last)}\n"
+          end
+
+          def convert_scalar(key, value, indent, is_in_array, is_last)
+            "#{indent}#{key} = \"#{value}\"#{trailing_comma(is_in_array, is_last)}\n"
+          end
+
+          def trailing_comma(is_in_array, is_last)
+            is_in_array && !is_last ? ',' : ''
+          end
+
+          def snake_case(str)
+            str.gsub(/([a-z])([A-Z])/, '\1_\2').gsub(/([A-Z])([A-Z][a-z])/, '\1_\2').downcase
+          end
+        end
+      end
+    end
+  end
+end
